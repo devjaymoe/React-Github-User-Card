679968db363552e9aa4eff8b094b738e589e02d8
Devin_Moore <d.j.moore209@gmail.com>
2020-04-07 16:43:06 -0400

    displaying user data from axios call

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import UserCard from './components/UserCard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { user: {} }
+  }
+
+  componentDidMount(){
+    axios
+      .get(`https://api.github.com/users/devjaymoe`)
+      .then(response => {
+        //console.log(response.data)
+        this.setState({
+          user: response.data
+        });
+      })
+      .catch(error => {
+        console.log('Error:', error)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GitHub User Card</h1>
+          <UserCard user={this.state.user}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ my-app/src/components/UserCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UserCard = props => {
+    //console.log(props.user)
+    return (
+        <div className='card'>
+            <img src={props.user.avatar_url} alt='users github profile picture'/>
+            <h3 className='name'>{props.user.name}</h3>
+            <p className='username'>{props.user.login}</p>
+            <p>Location: {props.user.location}</p>
+            <p>Profile: <a href={props.user.html_url}>{props.user.html_url}</a></p>
+            <p>Bio: {props.user.bio}</p>
+        </div>
+    )
+}
+
+export default UserCard;
